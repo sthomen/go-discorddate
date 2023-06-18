8e44c227e884b06269c2d4f1a93baaee3fda478c
Staffan Thomen <staffan@thomen.fi>
2023-06-18 13:09:15 +0300

    Rejigger the dateformat code to use a string index and implement command line flags

--- dateformat/dateformat.go
+++ dateformat/dateformat.go
@@ -3,16 +3,17 @@ package dateformat
 import (
 	"fmt"
 	"time"
+	"errors"
 )
 
 const (
-	FORMAT_R = iota
-	FORMAT_D
-	FORMAT_d
-	FORMAT_T
-	FORMAT_t
-	FORMAT_F
-	FORMAT_f
+	FORMAT_R = "R"
+	FORMAT_D = "D"
+	FORMAT_d = "d"
+	FORMAT_T = "T"
+	FORMAT_t = "t"
+	FORMAT_F = "F"
+	FORMAT_f = "f"
 )
 
 type dateFormat struct {
@@ -20,18 +21,36 @@ type dateFormat struct {
 	Name string
 }
 
-var formats = map[int]dateFormat{
-	0: dateFormat{"R", "in 2 hours"},
-	1: dateFormat{"D", "November 4, 2023"},
-	2: dateFormat{"d", "04/11/2023"},
-	3: dateFormat{"T", "11:28:27 AM"},
-	4: dateFormat{"t", "11:28 AM"},
-	5: dateFormat{"F", "Saturday, November 4, 2023 11:28:27 AM"},
-	6: dateFormat{"f", "4 November 2023 11:28"},
+var formats = map[string]dateFormat{
+	"R": dateFormat{"R", "in 2 hours"},
+	"D": dateFormat{"D", "November 4, 2023"},
+	"d": dateFormat{"d", "04/11/2023"},
+	"T": dateFormat{"T", "11:28:27 AM"},
+	"t": dateFormat{"t", "11:28 AM"},
+	"F": dateFormat{"F", "Saturday, November 4, 2023 11:28:27 AM"},
+	"f": dateFormat{"f", "4 November 2023 11:28"},
 }
 
-func Get(which int) dateFormat {
-	return formats[which]
+func Get(which string) (dateFormat, error) {
+	result, ok := formats[which]
+
+	var err error = nil
+
+	if !ok {
+		err = errors.New("Invalid format: " + which)
+	}
+
+	return result, err
+}
+
+func Formats() map[string]string {
+	var result = make(map[string]string)
+
+	for key,format := range formats {
+		result[key] = format.Name
+	}
+
+	return result
 }
 
 func (self *dateFormat) Render(date time.Time) string {
--- main.go
+++ main.go
@@ -3,10 +3,43 @@ package main
 import (
 	"fmt"
 	"time"
+	"flag"
+	"os"
 	"git.shangtai.net/staffan/go-discorddate/dateformat"
 )
 
 func main() {
-	var format = dateformat.Get(dateformat.FORMAT_t)
-	fmt.Printf("Test: %s", format.Render(time.Now()))
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [-f format] <date>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	var formatkey = flag.String("f", "t", "Rendering format")
+
+	flag.Parse()
+
+	var date time.Time
+
+	if flag.NArg() < 1 {
+		date = time.Now()
+	} else {
+		parsed, err := time.Parse(time.DateTime, flag.Arg(0))
+
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+
+		date = parsed
+	}
+
+
+	format, err := dateformat.Get(*formatkey)
+
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	println(format.Render(date))
 }
