1653f9df16c65485f6697ee26c574a91d8162007
Staffan Thomen <staffan@thomen.fi>
2023-06-25 18:17:52 +0300

    Add a bit of logic that automatically runs the program in gui-mode if a binary ending with -gui is run

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"time"
 	"flag"
 	"os"
+	"regexp"
 
 	"git.shangtai.net/staffan/go-discorddate/internal/dateformat"
 	"git.shangtai.net/staffan/go-discorddate/internal/gui"
 )
 
 const DATETIME="2006-01-02 15:04:05"
+
+func ranWithGui() bool {
+	match, _ := regexp.Match("-gui(|.exe)", []byte(os.Args[0]))
+	return match
+}
 
 func main() {
 	flag.Usage = func() {
@@ -23,7 +29,7 @@ func main() {
 	}
 
 	var formatkey = flag.String("f", "t", "Rendering format")
-	var use_gui = flag.Bool("g", false, "Start GUI")
+	var use_gui = flag.Bool("g", ranWithGui(), "Start GUI")
 
 	flag.Parse()
 
