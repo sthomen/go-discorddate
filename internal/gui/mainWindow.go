2d3476ef98b0f0d2a67fc5cd6f006a371a731abd
Staffan Thomen <staffan@thomen.fi>
2023-06-25 14:03:36 +0300

    Add an internal context to the gui that holds the state and a preview label that actually updates when the ui changes.

--- internal/gui/mainWindow.go
+++ internal/gui/mainWindow.go
@@ -10,14 +10,46 @@ import (
 	"git.shangtai.net/staffan/go-discorddate/internal/dateformat"
 )
 
-func MainWindow(time time.Time, format dateformat.DateFormat) {
+// App UI context
+type _context struct {
+	date time.Time
+	format dateformat.DateFormat
+	formatWidget *widget.Select
+	previewWidget *widget.Label
+}
+
+var context = _context{}
+
+func MainWindow(date time.Time, format dateformat.DateFormat) {
+	context.date = date
+	context.format = format
+
 	app := app.New()
 	w := app.NewWindow("Discord Date Tool")
 
-	formatWidget := widget.NewSelect(dateformat.FormatsOptions(), func (value string) {
-		print(value)
-	})
+	context.formatWidget = widget.NewSelect(dateformat.FormatsOptions(), updateUi)
+	context.previewWidget = widget.NewLabel("")
 
-	w.SetContent(container.NewVBox(formatWidget))
+	w.SetContent(container.NewVBox(
+		context.formatWidget,
+		context.previewWidget,
+	))
+
+	// Set initial value, but this has to be done after SetContent or fyne will
+	// panic.
+	context.formatWidget.SetSelected(context.format.Name)
+
 	w.ShowAndRun()
+}
+
+func updateUi(_ string) {
+	var index = context.formatWidget.SelectedIndex()
+
+	format, err := dateformat.ByIndex(index)
+
+	if err == nil {
+		context.format = format
+	}
+
+	context.previewWidget.SetText(context.format.Render(context.date))
 }
