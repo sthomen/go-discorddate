f89e564fe120200c28594f247356754ede2e840b
Staffan Thomen <staffan@thomen.fi>
2023-08-05 22:52:58 +0300

    Just use windres instead of go-windres and go generate

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-go generate
+windres go-discorddate.rc -o winmanifest_windows_386.syso -O coff
+windres go-discorddate.rc -o winmanifest_windows_amd64.syso -O coff
 go build 
 go build -ldflags -H=windowsgui -o go-discorddate-gui.exe
